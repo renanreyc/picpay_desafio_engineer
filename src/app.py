f15d94d1749e5adec8caf271fdb9533a75b094f7
Renan Rey Costa Rodrigues <renan.costarodrigues-ext@ab-inbev.com>
2025-01-22 17:13:53 -0300

    feature/imp-models-and-schemas

--- src/app.py
+++ src/app.py
@@ -30,6 +30,15 @@ def create_user(user: UserSchema):
 def read_users():
     return {'users': database}
 
+@app.get('/users/{user_id}', response_model=UserPublic)
+def read_user(user_id: int):
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(
+            status_code = HTTPStatus.NOT_FOUND, detail= 'User not found'
+        )
+    
+    return database[user_id - 1]
+
 @app.put('/users/{user_id}', response_model=UserPublic)
 def update_user(user_id: int, user: UserSchema):
     if user_id < 1 or user_id > len(database):
--- src/schemas.py
+++ src/schemas.py
@@ -12,7 +12,6 @@ class UserDB(UserSchema):
     id: int
 
 class UserPublic(BaseModel):
-    id: int
     username: str
     email: EmailStr
 
