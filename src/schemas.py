d36b550ed6854760feee5c5f27924c04cff463c8
Renan Rey Costa Rodrigues <renan.costarodrigues-ext@ab-inbev.com>
2025-01-25 16:47:15 -0300

    feature/implement-authentication

--- src/schemas.py
+++ src/schemas.py
@@ -16,3 +16,7 @@ class UserPublic(BaseModel):
 
 class UserList(BaseModel):
     users: list[UserPublic]
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
--- tests/test_app.py
+++ tests/test_app.py
@@ -17,7 +17,7 @@ def test_create_user(client):
                 json={
         'username':'test',
         'email': 'test@test.com',
-        'password': 'password'
+        'password': 'Test@1234'
     })
 
     assert response.status_code == HTTPStatus.CREATED
@@ -54,16 +54,20 @@ def test_create_user_should_return_400_email_exists(client, user):
     assert response.status_code == HTTPStatus.BAD_REQUEST
     assert response.json() == {'detail': 'Email already exists'}
 
-def test_delete_user_should_return_not_found(client):
-    response = client.delete('/users/666')
+def test_delete_user_should_return_not_found(client, token):
+    response = client.delete(
+        '/users/666',
+        headers = { 'Authorization': f'Bearer {token}' }
+    )
 
     assert response.status_code == HTTPStatus.NOT_FOUND
     assert response.json() == {'detail': 'User not found'}
 
 
-def test_update_user_should_return_not_found(client):
+def test_update_user_should_return_not_found(client, token):
     response = client.put(
         '/users/666',
+        headers = { 'Authorization': f'Bearer {token}' },
         json={
             'username': 'bob',
             'email': 'bob@example.com',
@@ -94,15 +98,16 @@ def test_read_users_with_user(client, user):
     assert response.status_code == HTTPStatus.OK
     assert response.json() == { 'users': [user_schema] }
 
-def test_update_user(client, user):
+def test_update_user(client, user, token):
     UserPublic.model_validate(user).model_dump()
 
     response = client.put(
-        '/users/1',
-        json={
+        f'/users/{ user.id }',
+        headers = { 'Authorization': f'Bearer {token}' },
+        json = {
             'id': 1,
             'username':'testput',
-            'password': 'newpass',
+            'password': 'Test@1234',
             'email': 'test@test.com'
         }
     )
@@ -113,10 +118,25 @@ def test_update_user(client, user):
             'email': 'test@test.com'
         }
 
-def test_delete_user(client, user):
+def test_delete_user(client, user, token):
     UserPublic.model_validate(user).model_dump()
     
-    response = client.delete('/users/1')
+    response = client.delete(
+        f'/users/{ user.id }',
+        headers = { 'Authorization': f'Bearer {token}' }
+    )
 
     assert response.json() == {'message': 'User deleted!'}
 
+
+def test_get_token(client, user):
+    response = client.post(
+        '/auth/token',
+        data= { 'username': user.email,'password': user.clean_password }
+    )
+
+    token = response.json()
+
+    assert response.status_code == HTTPStatus.OK
+    assert token['token_type'] == 'Bearer'
+    assert 'access_token' in token
